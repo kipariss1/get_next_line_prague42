134829839194f7504a302980bbeb925562b09e30
kipariss1 <kpassudin@gmail.com>
2024-09-13 12:57:22 +0200

    TODO: fix bugs

--- get_next_line.c
+++ get_next_line.c
@@ -6,46 +6,53 @@
 /*   By: krassudi <krassudi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 10:31:12 by krassudi          #+#    #+#             */
-/*   Updated: 2024/09/11 00:57:06 by krassudi         ###   ########.fr       */
+/*   Updated: 2024/09/13 12:52:21 by krassudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void init_stash(char **stash){
-    if (*stash)
-	{
-        free(*stash);
-	}
-    *stash = malloc(sizeof(char) * 1);
-    *stash[0] = '\0';
-}
+static void init_buff(char **buff, char type){
+    size_t buff_size;
 
-static void init_buff(char **buff) {
-    *buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (*buff)
+        free(*buff);
+    buff_size = (type == 'b') ? BUFFER_SIZE + 1 : 1;
+    *buff = malloc(sizeof(char) * buff_size);
     if (!*buff)
         return ;
-    ft_memset(*buff, '\0', BUFFER_SIZE + 1);
+    ft_memset(*buff, '\0', buff_size);
+}
+
+static void init_buffs(char **buff, char **res, char **before_nl, char **stash){
+    *buff = NULL;
+    *res = NULL;
+    *before_nl = NULL;
+    *stash = NULL;
+}
+
+static void free_buff(char **buff){
+    if (*buff)
+        free(*buff);
+    *buff = NULL;
 }
 
 static int  read_file(int fd, char **stash, char **buff, char **res) {
     int			read_chars;
 
     if (!*stash)
-        init_stash(stash);
+        init_buff(stash, 's');
     if (ft_strchr(*stash, '\n')){
         *buff = ft_strdup(*stash);
-        init_stash(stash);
+        init_buff(stash, 's');
         return (1);
     }
-    init_buff(buff);
+    init_buff(buff, 'b');
     read_chars = read(fd, *buff, BUFFER_SIZE);
     if (read_chars <= 0  && ft_strlen(*stash) > 0) {
         *res = ft_strdup(*stash);
-        free(*buff);
-        *buff = NULL;
-        free(*stash);
-        *stash = NULL;
+        free_buff(buff);
+        free_buff(stash);
         return (0);
     }
     else if (read_chars <= 0  && !ft_strlen(*stash)) {
@@ -64,26 +71,24 @@ char	*get_next_line(int fd)
 
     if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+    init_buffs(&buff, &res, &before_nl, &stash);
 	while (read_file(fd, &stash, &buff, &res))
 	{
 		if (ft_strchr(buff, '\n'))
 		{
 			before_nl = substr(buff, 0, ft_strchr(buff, '\n') + 1 - buff);
 			res = ft_strjoin(stash, before_nl);
-            free(before_nl);
-            before_nl = NULL;
-			free(stash);
-            stash = NULL;
+            free_buff(&before_nl);
+			free_buff(&stash);
 			stash = substr(buff, ft_strchr(buff, '\n') - buff + 1,
 					ft_strlen(buff));
-			free(buff);
-            buff = NULL;
+			free_buff(&buff);
             break;
 		}
 		else
 		{
 			stash = ft_strjoin(stash, buff);
-			free(buff);
+			free_buff(&buff);
 		}
 	}
 	return (res);
